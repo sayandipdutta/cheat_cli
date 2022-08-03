f20178170dec6982caf9f788b01af8bcde66cbe2
sayandip199309@gmail.com <sayandip199309@gmail.com>
2022-08-03 21:16:23 +0530

    Return both list of args, and query from format_args

--- cheat.py
+++ cheat.py
@@ -2,8 +2,23 @@ import argparse
 import subprocess
 from enum import Enum, auto
 
+def load_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Search cheat.sh")
+    group = parser.add_mutually_exclusive_group()
+    add_args = parser.add_argument
+    add_args("topic", metavar="L", help="The topic to search for.")
+    group.add_argument("-s", "--subtopic", metavar="q", help="Search string")
+    group.add_argument("-k", "--kwd", help="Keywords", nargs='*')
+    group.add_argument("-c", "--cmd", choices=list(Commands), help=f"One of the following commands: {', '.join(Commands)}")
+    add_args("-o", "--options", nargs='*', choices=list(Options))
+    add_args("--style")
+    add_args("--search_opts", nargs=True, choices=list(SearchOpts))
+
+    args = parser.parse_args()
+    return args
+
 class AutoName(Enum):
-    def _generate_next_value_(name, start, count, last_value):
+    def _generate_next_value_(name, *_):
         return name
 
 class Commands(str, AutoName):
@@ -26,7 +41,6 @@ class SearchOpts(str, AutoName):
     r = auto()
 
 class Cheat:
-
     @staticmethod
     def format_args(
             topic: str = "",
@@ -37,7 +51,7 @@ class Cheat:
             style: str = None,
             search_opts: list[SearchOpts] = None,
             kwd: list[str] = None
-            ) -> str:
+            ) -> tuple[list[str], str]:
         topic = "/"+ "+".join(topic.strip().split())
         subtopic = '/' + "+".join(subtopic.strip().split()) if subtopic else ""
         cmds = '/:%s' % cmd if cmd else ''
@@ -45,28 +59,14 @@ class Cheat:
         style = ("&style=%s" % style) if style else ""
         search_opt = ("/" + "".join(search_opts)) if search_opts else ''
         keywords = ("/~" + "~".join(kwd)) if kwd else ""
-        query = topic + subtopic + keywords + cmds + opts + style + search_opt
-        return query
+        args = [topic, subtopic, keywords, cmds, opts, style, search_opt]
+        query = ''.join(args)
+        return args, query
 
-
-def load_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="Search cheat.sh")
-    group = parser.add_mutually_exclusive_group()
-    add_args = parser.add_argument
-    add_args("topic", metavar="L", help="The topic to search for.")
-    group.add_argument("-s", "--subtopic", metavar="q", help="Search string")
-    group.add_argument("-k", "--kwd", help="Keywords", nargs='*')
-    group.add_argument("-c", "--cmd", choices=list(Commands), help=f"One of the following commands: {', '.join(Commands)}")
-    add_args("-o", "--options", nargs='*', choices=list(Options))
-    add_args("--style")
-    add_args("--search_opts", nargs=True, choices=list(SearchOpts))
-
-    args = parser.parse_args()
-    return args
 
 if __name__ == "__main__":
     args = load_args()
-    query = Cheat.format_args(
+    formatted_args, query = Cheat.format_args(
             topic=args.topic,
             subtopic=args.subtopic,
             cmd=args.cmd,
